a2b112917cde7c00ff39338863229f810f09afec
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-01-31 17:14:27 +0000

    Chapter 8 - Touch in and touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,33 @@
 class Oystercard 
   attr_accessor :balance
+  attr_accessor :journey_status
+
   MAXIMUM_CREDIT = 90
 
   def initialize
     @balance = 0
+    @journey_status = false
   end
 
   def topup(credit)
     fail "Maximum balance #{MAXIMUM_CREDIT}" if (@balance + credit) > MAXIMUM_CREDIT
     @balance += credit
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+    @journey_status = true
+  end
+
+  def touch_out
+    @journey_status = false
+  end
+
+  def in_journey?
+    @journey_status
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,29 @@ describe Oystercard do
     subject.topup(Oystercard::MAXIMUM_CREDIT)
     expect{ subject.topup(5) }.to raise_error("Maximum balance #{Oystercard::MAXIMUM_CREDIT}")
   end
+
+  it 'should deduct the cost of the fare from the balance' do
+    expect(subject).to respond_to(:deduct)
+  end
+
+  it 'should decrease the balance by fare amount' do
+    subject.topup(10)
+    subject.deduct(5)
+    expect(subject.balance).to eq (5)
+  end
+
+  it 'should check if in a journey' do
+    expect(subject.in_journey?).to eq false
+  end
+
+  it 'should confirm that we are in a journey' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'should confirm that we are no longer in a journey' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
 end
