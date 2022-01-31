798635e2ca67ff429da47970e65e288253049c52
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-31 16:04:25 +0000

    Chapter 5 & 6 completed.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,14 @@
 class Oystercard 
   attr_accessor :balance
+  MAXIMUM_CREDIT = 90
 
   def initialize
     @balance = 0
+  end
+
+  def topup(credit)
+    fail "Maximum balance #{MAXIMUM_CREDIT}" if (@balance + credit) > MAXIMUM_CREDIT
+    @balance += credit
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,17 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do
- it 'should show money on a card' do
-  expect(subject).to respond_to(:balance)
- end
+  it 'should show money on a card' do
+    expect(subject).to respond_to(:balance)
+  end
+
+  it 'should increase the balance by topup amount' do
+    subject.topup(5)
+    expect(subject.balance).to eq (5)
+  end
+
+  it 'should not allow the user to top up above £90' do
+    subject.topup(Oystercard::MAXIMUM_CREDIT)
+    expect{ subject.topup(5) }.to raise_error("Maximum balance #{Oystercard::MAXIMUM_CREDIT}")
+  end
 end
